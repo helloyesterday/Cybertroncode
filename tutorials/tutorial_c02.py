fa5fba2a6c3fc90889e7492a5635310df31a58c5
helloyesterday <yangyi@szbl.ac.cn>
2023-01-17 09:49:52 +0800

    update copyright to 2023

--- tutorials/tutorial_c02.py
+++ tutorials/tutorial_c02.py
@@ -1,4 +1,4 @@
-# Copyright 2021-2022 @ Shenzhen Bay Laboratory &
+# Copyright 2021-2023 @ Shenzhen Bay Laboratory &
 #                       Peking University &
 #                       Huawei Technologies Co., Ltd
 #
@@ -64,6 +64,7 @@ if __name__ == '__main__':
         activation='swish',
         max_cycles=10,
         length_unit='nm',
+        coupled_interaction=True,
     )
 
     readout = AtomwiseReadout(
